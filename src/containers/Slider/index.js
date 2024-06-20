e822c884aa862df4071caef82d370477d7a5875a
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-06-20 18:06:17 +0200

    fix select filter list

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,60 +1,79 @@
-import { useEffect, useState } from "react";
-import { useData } from "../../contexts/DataContext";
-import { getMonth } from "../../helpers/Date";
+import { useEffect, useState } from 'react';
+import { useData } from '../../contexts/DataContext';
+import { getMonth } from '../../helpers/Date';
 
-import "./style.scss";
+import './style.scss';
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus?.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  ) || [];
+	const { data } = useData();
+	const [index, setIndex] = useState(0);
+	const [selectedRadio, setSelectedRadio] = useState(0);
 
-  useEffect(() => {
+	const byDateDesc =
+		data?.focus?.sort((eventA, eventB) =>
+			new Date(eventA.date) < new Date(eventB.date) ? -1 : 1
+		) || [];
+
+	const handleRadioInput = (radioIndex) => {
+		setSelectedRadio(radioIndex);
+		setIndex(radioIndex);
+	};
+  
+  const changeIndex = () => {
     if (byDateDesc.length === 0) return;
 
-    setTimeout(() => {
-      setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
-    }, 5000);
+    setIndex((current) =>
+      current < byDateDesc.length - 1 ? current + 1 : 0
+    );
 
-  }, [index, byDateDesc.length]);
+    setSelectedRadio((current) =>
+      current < byDateDesc.length - 1 ? current + 1 : 0
+    );
+  };
 
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((eventData, eventIndex) => (
-        <div key={eventData.title}>
-          <div
-            className={`SlideCard SlideCard--${
-              index === eventIndex ? "display" : "hide"
-            }`}
-          >
-            <img src={eventData.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{eventData.title}</h3>
-                <p>{eventData.description}</p>
-                <div>{getMonth(new Date(eventData.date))}</div>
-              </div>
-            </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIndex) => (
-                <input
-                  key={`${eventData.title}-${radioIndex + 1}`}
-                  type="radio"
-                  name="radio-button"
-                  onChange={() => setIndex(radioIndex)}
-                  checked={index === radioIndex}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+	useEffect(() => {
+
+		const interval = setTimeout(() => changeIndex(), 5000);
+
+		return () => clearTimeout(interval);
+	}, [index, byDateDesc]);
+
+	return (
+		<div className='SlideCardList'>
+			{byDateDesc.map((eventData, eventIndex) => (
+				<div key={eventData.title}>
+					<div
+						className={`SlideCard SlideCard--${
+							index === eventIndex ? 'display' : 'hide'
+						}`}>
+						<img src={eventData.cover} alt='forum' />
+						<div className='SlideCard__descriptionContainer'>
+							<div className='SlideCard__description'>
+								<h3>{eventData.title}</h3>
+								<p>{eventData.description}</p>
+								<div>{getMonth(new Date(eventData.date))}</div>
+							</div>
+						</div>
+					</div>
+					<div className='SlideCard__paginationContainer'>
+						<div className='SlideCard__pagination'>
+							{byDateDesc.map((_, radioIndex) => (
+								<input
+									key={`${eventData.title}-${radioIndex + 1}`}
+									type='radio'
+									name='radio-button'
+									onChange={() => {
+										handleRadioInput(radioIndex)
+									}}
+									checked={selectedRadio === radioIndex}
+								/>
+							))}
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Slider;
